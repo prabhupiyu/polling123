71d3a2468eef2bfa502400b8a7be3d179f5f5e86
Keshav Kelkar <keshav_kelkar@persistent.co.in>
2016-08-26 11:11:24 +0530

    Admin Pannel Changes -- Sairaj

--- views/javascripts/old/chartangular.js
+++
@@ -1,5 +0,0 @@
-/*angular.module("polls", ["chart.js"]).controller("PieCtrl", function ($scope) {
-  $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-  $scope.data = [300, 500, 100];
-});
-           */
